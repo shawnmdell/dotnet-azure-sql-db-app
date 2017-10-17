bd4c26badfc19d95541e54bd8e971a175f4fc4d2
shawnmdell <shawnmdell@hotmail.com>
2017-10-17 10:52:58 -0400

    Added entityFramework tag information to App.config file

--- DotNetAppSqlDb/Controllers/TodosController.cs
+++ DotNetAppSqlDb/Controllers/TodosController.cs
@@ -6,7 +6,8 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using DotNetAppSqlDb.Models;using System.Diagnostics;
+using DotNetAppSqlDb.Models;
+using System.Diagnostics;
 
 namespace DotNetAppSqlDb.Controllers
 {
